cd9f01745ea3495b2b6eca477c18145a99adfc2f
White_Panther <99096313+White-Panther420@users.noreply.github.com>
2023-11-02 11:12:53 -0700

    Created modal
    I made a modal to let the player place their ships. It's mostly working, just need to fix eventListener issues and ensure all ships placed are valid placements

--- src/LoadPage.js
+++ src/LoadPage.js
@@ -8,6 +8,8 @@ import BGMusic1 from "./Assets/Music/Background Music/Overtaken.mp3";
 import BGMusic2 from "./Assets/Music/Background Music/VeryStrongest.mp3";
 import SoundIcon from "./Assets/Images/sound.png";
 
+import GameBoard from ".";
+
 const contentDiv = document.getElementById("content");
 contentDiv.style.backgroundRepeat = "no-repeat";
 contentDiv.style.backgroundSize = "cover";
@@ -32,23 +34,136 @@ const createImage = (ImportedImg, className) => {
   return myImg;
 };
 
-const createGameBoardGUI = (playerName) => {
-  const gameBoard = createElement("div", `gameBoard ${playerName}`);
-  for (let i = 0; i < 10; i += 1) { // Y coordinate
+const eraseSquares = () => {
+  const allPaintedSqaures = document.querySelectorAll(".painted");
+  allPaintedSqaures.forEach((paintedSquare) => {
+    // eslint-disable-next-line no-param-reassign
+    paintedSquare.classList.remove("painted");
+  });
+};
+
+const createGameBoardGUI = (placementBoard = false, ships = "") => {
+  const gameBoard = createElement("div", "gameBoard");
+
+  for (let i = 0; i < 10; i += 1) {
     // Make it easier to give squares (x, y) coordinates
     const rowDiv = createElement("div", "rowDiv");
-    rowDiv.setAttribute("data", i);
+    rowDiv.setAttribute("data-state", i);
     gameBoard.appendChild(rowDiv);
     for (let j = 0; j < 10; j += 1) { // X coordinate
       const square = createElement("div", "square player1");
-      square.setAttribute("data", `(${j}, ${i}`);
+      square.setAttribute("data-state", `(${j}, ${i})`);
+      // Add a different eventListener for a board that only places ships
+      if (placementBoard) {
+        square.addEventListener("mouseenter", () => {
+          // Get (x, y) coordinate of square as strings in an array
+          const squareCoordinateStr = square.getAttribute("data-state").match(/\d+/g);
+          // Turn those strings into ints
+          const squareCoordinate = squareCoordinateStr.map((match) => parseInt(match, 10));
+          const orientation = document.querySelector(".rotateShipBtn").getAttribute("id");
+
+          // Paint squares equivalent to ship length
+          for (let k = 0; k < Object.values(ships)[0]; k += 1) {
+            if (orientation === "horizontal") {
+              const dataState = `(${squareCoordinate[0] + k}, ${squareCoordinate[1]})`;
+              const placementSquare = document.querySelector(`[data-state="${dataState}"]`);
+              if (placementSquare === null || placementSquare.classList.contains("shipPlaced")) {
+                // Visual message letting the user know they're attempting to place a ship
+                // on an invalid spot on the board
+                eraseSquares();
+              } else {
+                placementSquare.classList.add("painted");
+              }
+            } else {
+              const dataState = `(${squareCoordinate[0]}, ${squareCoordinate[1] + k})`;
+              const placementSquare = document.querySelector(`[data-state="${dataState}"]`);
+              if (placementSquare === null || placementSquare.classList.contains("shipPlaced")) {
+                eraseSquares();
+              } else {
+                placementSquare.classList.add("painted");
+              }
+            }
+          }
+        });
+        square.addEventListener("mouseleave", () => {
+          // Clear squares of bg color
+          eraseSquares();
+        });
+        square.addEventListener("click", () => {
+          const firstKeyName = Object.keys(ships)[0];
+          const pointAStr = square.getAttribute("data-state").match(/\d+/g);
+          // Turn those strings into ints
+          const pointA = pointAStr.map((match) => parseInt(match, 10));
+          const orientation = document.querySelector(".rotateShipBtn").getAttribute("id");
+
+          GameBoard.placeShip(pointA, orientation, firstKeyName);
+          const paintedSquares = document.querySelectorAll(".painted");
+          paintedSquares.forEach((paintedSquare) => {
+            paintedSquare.classList.add("shipPlaced");
+          });
+
+          // eslint-disable-next-line no-param-reassign
+          const keys = Object.keys(ships);
+          const firstKey = keys[0];
+          delete ships[firstKey];
+
+          console.log(ships);
+          // All ships are placed
+          if (Object.keys(ships).length === 0) {
+            const modal = document.querySelector(".modal");
+            modal.style.display = "none";
+          }
+        });
+      }
       rowDiv.appendChild(square);
     }
   }
   return gameBoard;
 };
 
+// Displays the board where player will place their ships
+const displayPlacementBoardModal = () => {
+  const ships = {
+    carrier: 5,
+    battleship: 4,
+    cruiser: 4,
+    submarine: 3,
+    destroyer: 2,
+  };
+
+  const modal = createElement("modal", "modal");
+  const welcomeHeader = createElement("h1", "welcomeHeader");
+  welcomeHeader.textContent = "Welcome to One Piece Battleship!";
+  const welcomeMsg = createElement("p", "welcomeMsg");
+  welcomeMsg.textContent = "Oh No! Adimral Akainu has targeted your ships with his new ship-targeting system! Place your ships strategically to escape his wrath and sink those Navy Ships!";
+
+  const placeShipMsg = createElement("p", "placeSHipMsg");
+  placeShipMsg.textContent = "Place your ";
+
+  const rotateShipBtn = createElement("button", "rotateShipBtn");
+  rotateShipBtn.setAttribute("id", "horizontal");
+  rotateShipBtn.textContent = "Rotate Ship";
+  rotateShipBtn.addEventListener("click", () => {
+    if (rotateShipBtn.getAttribute("id") === "horizontal") {
+      rotateShipBtn.setAttribute("id", "vertical");
+    } else {
+      rotateShipBtn.setAttribute("id", "horizontal");
+    }
+  });
+  const placementBoard = createGameBoardGUI(true, ships);
+
+  modal.appendChild(welcomeHeader);
+  modal.appendChild(welcomeMsg);
+  modal.appendChild(placeShipMsg);
+  modal.appendChild(rotateShipBtn);
+  modal.appendChild(placementBoard);
+
+  contentDiv.appendChild(modal);
+};
+
 const createPage = () => {
+  displayPlacementBoardModal();
+
   const leftImgDiv = createElement("div", "leftImgDiv");
   const luffyImg = createImage(Luffy, "LuffyImg");
   leftImgDiv.appendChild(luffyImg);
@@ -64,18 +179,12 @@ const createPage = () => {
   titleContainer.appendChild(battleshipLogo);
 
   // Background music
-  // const overtakenAudio = createElement("audio", "audio");
-  // overtakenAudio.src = BGMusic1;
+  const overtakenAudio = createElement("audio", "audio");
+  overtakenAudio.src = BGMusic1;
   const veryStrongestAudio = createElement("audio", "audio");
   veryStrongestAudio.src = BGMusic2;
-  // gameContainer.appendChild(overtakenAudio);
+  gameContainer.appendChild(overtakenAudio);
   gameContainer.appendChild(veryStrongestAudio);
-  const sound = createImage(SoundIcon, "sound");
-  gameContainer.appendChild(sound);
-
-  sound.addEventListener("click", () => {
-    veryStrongestAudio.play();
-  });
 
   const gameBoardContainers = createElement("div", "gameBoardContainers");
 
@@ -94,7 +203,7 @@ const createPage = () => {
   const AIInfoContainer = createElement("div", "AIInfoContainer flex");
   const AIToken = createElement("div", "AIToken");
   const AINameP = createElement("p", "AIName");
-  AINameP.textContent = "Admiral Sakazuki Akainu";
+  AINameP.textContent = "Admiral Sakazuki";
   AIInfoContainer.appendChild(AIToken);
   AIInfoContainer.appendChild(AINameP);
   AIGameBoardContainer.appendChild(AIInfoContainer);
@@ -104,9 +213,13 @@ const createPage = () => {
   gameBoardContainers.appendChild(AIGameBoardContainer);
 
   const actionButtonsDiv = createElement("div", "actionButtonsDiv flex");
-  const restartBtn = createElement("button", "restartBtn");
   const quitGameBtn = createElement("button", "quitGameBtn");
-  actionButtonsDiv.appendChild(restartBtn);
+  quitGameBtn.textContent = "Quit Game";
+  const sound = createImage(SoundIcon, "sound");
+  sound.addEventListener("click", () => {
+    veryStrongestAudio.play();
+  });
+  actionButtonsDiv.appendChild(sound);
   actionButtonsDiv.appendChild(quitGameBtn);
 
   gameContainer.appendChild(titleContainer);
@@ -121,5 +234,4 @@ const createPage = () => {
   contentDiv.appendChild(gameContainer);
   contentDiv.appendChild(rightImgDiv);
 };
-
 export default createPage;
--- src/index.js
+++ src/index.js
@@ -2,29 +2,29 @@
 import "./Styles/styles.css";
 import createPage from "./LoadPage";
 
-const Battleship = (length) => {
+const Battleship = (shipType) => {
   let numTimesHit = 0;
   let sunk = false;
-  const lengthOfShip = length;
-  //   switch (shipType.toLowerCase()) {
-  //     case "carrier":
-  //       lengthOfShip = 5;
-  //       break;
-  //     case "battleship":
-  //       lengthOfShip = 4;
-  //       break;
-  //     case "cruiser":
-  //       lengthOfShip = 3;
-  //       break;
-  //     case "submarine":
-  //       lengthOfShip = 3;
-  //       break;
-  //     case "destroyer":
-  //       lengthOfShip = 3;
-  //       break;
-  //     default:
-  //       break;
-  //   }
+  let lengthOfShip;
+  switch (shipType) {
+    case "carrier":
+      lengthOfShip = 5;
+      break;
+    case "battleship":
+      lengthOfShip = 4;
+      break;
+    case "cruiser":
+      lengthOfShip = 3;
+      break;
+    case "submarine":
+      lengthOfShip = 3;
+      break;
+    case "destroyer":
+      lengthOfShip = 3;
+      break;
+    default:
+      break;
+  }
 
   const getLengthOfShip = () => lengthOfShip;
   const getSunkState = () => sunk;
@@ -49,7 +49,7 @@ const player = (name) => {
   return { getName, getNumShips };
 };
 
-const gameBoard = (() => {
+const GameBoard = (() => {
   const createGameBoard = () => {
     const board = [];
     for (let i = 0; i < 10; i += 1) {
@@ -63,18 +63,9 @@ const gameBoard = (() => {
 
   const board = createGameBoard();
 
-  const calcShipLength = (pointA, pointB) => {
-    const xLength = pointB[0] - pointA[0]; // X2 - X1
-    const yLength = pointB[1] - pointA[1]; // Y2 - Y1
-    const distance = Math.sqrt((xLength * xLength) + (yLength * yLength));
-    return distance + 1; // The first square needs to be included;
-  };
-  const placeShip = (pointA, pointB, orientation) => {
-    // eslint-disable-next-line no-param-reassign
-    orientation = orientation.toLowerCase();
-    const shipLength = calcShipLength(pointA, pointB);
-    const newShip = Battleship(shipLength);
-    for (let i = 0; i < shipLength; i += 1) {
+  const placeShip = (pointA, orientation, shipName) => {
+    const newShip = Battleship(shipName);
+    for (let i = 0; i < newShip.getLengthOfShip(); i += 1) {
       // Flipped orientation since array is flipped
       if (orientation === "horizontal") {
         board[pointA[1]][pointA[0] + i] = newShip;
@@ -99,17 +90,15 @@ const gameBoard = (() => {
   };
 
   return {
-    createGameBoard, calcShipLength, placeShip, recieveAttack,
+    createGameBoard, placeShip, recieveAttack,
   };
 })();
 
 const gameController = (() => {
-  const board = gameBoard.createGameBoard();
+  const board = GameBoard.createGameBoard();
   const players = [player("Player1"), player("AI")];
 })();
 
-gameBoard.placeShip([3, 4], [7, 4], "horizontal");
-gameBoard.recieveAttack([6, 4]);
 createPage();
 
-// module.exports = gameBoard;
+export default GameBoard;
